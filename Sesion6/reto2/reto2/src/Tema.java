c1b609b45e3c3dd1cc039087cd2458ef043431f7
AnahiDzulCan <anahidcan@gmail.com>
2025-05-16 23:16:02 -0600

    Sesion6

---
+++ Sesion6/reto2/reto2/src/Tema.java
@@ -0,0 +1,27 @@
+class Tema implements Comparable<Tema> {
+    String titulo;
+    int prioridad;
+
+    public Tema(String titulo, int prioridad) {
+        this.titulo = titulo;
+        this.prioridad = prioridad;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public int compareTo(Tema otro) {
+        return this.titulo.compareToIgnoreCase(otro.titulo);
+    }
+
+    @Override
+    public String toString() {
+        return "Tema: " + titulo + " (Prioridad: " + prioridad + ")";
+    }
+}
